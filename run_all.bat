a7609b4b4a204c553a16b9de19cd36e898b90ea3
SaraMoazez <62360527+SaraMoazez@users.noreply.github.com>
2022-05-12 16:08:42 +0200

    Update run_all.bat

--- run_all.bat
+++ run_all.bat
@@ -9,7 +9,7 @@ timeout /t 4
 start cmd /k cargo run --bin id_2203 -- --pid 1 --peers 2 3
 start cmd /k cargo run --bin id_2203 -- --pid 2 --peers 1 3
 start cmd /k cargo run --bin id_2203 -- --pid 3 --peers 1 2
-::start cmd /k cargo run --bin iD2203 -- --pid 4 --peers 1 2 3 5
-::start cmd /k cargo run --bin iD2203 -- --pid 5 --peers 1 2 3 4
+::start cmd /k cargo run --bin id_2203 -- --pid 4 --peers 1 2 3 5
+::start cmd /k cargo run --bin id_2203 -- --pid 5 --peers 1 2 3 4
 
 cls
